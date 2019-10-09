b0c1affe668e302b94c1a42c90feb4bf478bfc3d
vedmas <07011984t@mail.ru>
2019-10-09 09:07:48 +0500

    Написал методы по разбивке файла на байт пакеты, чборки в единный байт массив и сохранении на диск. Нужно прописать динамическую размерность пакетов байт

--- src/main/java/ru/MyCloud/Main.java
+++ src/main/java/ru/MyCloud/Main.java
@@ -19,24 +19,34 @@ public class Main {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            byte[] newData = mergeFile(splitFile(data));
+        System.out.println(newData.length);
 
-            int[] n1 = new int[] {1, 2, 3};
-            int[] n2 = new int[] {4, 5};
-            n2 = Arrays.copyOf(n2, n1.length + n2.length);
-        System.arraycopy(n1, 0, n2, 2, n1.length);
-        System.out.println("n2.length = " + n2.length);
-        for (int i : n2) {
-            System.out.println(i);
+
+//            int[] n1 = new int[] {1, 2, 3};
+//            int[] n2 = new int[] {4, 5};
+//            n2 = Arrays.copyOf(n2, n1.length + n2.length);
+//        System.arraycopy(n1, 0, n2, 2, n1.length);
+//        System.out.println("n2.length = " + n2.length);
+//        for (int i : n2) {
+//            System.out.println(i);
+//        }
+
+
+        try {
+            Files.write(Paths.get("./client_storage/6.txt"), newData, StandardOpenOption.CREATE);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
-
-//            Files.write(Paths.get("./client_storage/6.txt"), data, StandardOpenOption.CREATE);
 
     }
 
     private static List<byte[]> splitFile(byte[] data) {
         List<byte[]> list = new ArrayList<>();
+
+        if(data.length > 5) {
+
+        }
         byte[] dataTemp = new byte[5];
         int marker = -1;
         for (byte datum : data) {
@@ -47,27 +57,19 @@ public class Main {
                 dataTemp = new byte[5];
             }
             dataTemp[marker] = datum;
-
         }
         list.add(dataTemp);
         return list;
     }
 
     private static byte[] mergeFile(List<byte[]> list) {
-        int marker = 0;
-        byte[] newData = new byte[5];
-            for (byte[] bytes : list) {
-                if(marker == newData.length -1) {
-
-                }
-                    for (byte aByte : bytes) {
-                        newData[marker] = aByte;
-                        marker++;
-                    }
-
-            }
-
-
+        byte[] newData = new byte[0];
+        int  currentLength;
+        for (byte[] bytes : list) {
+            currentLength = newData.length;
+            newData = Arrays.copyOf(newData, newData.length + bytes.length);
+            System.arraycopy(bytes, 0, newData, currentLength, bytes.length);
+        }
         return newData;
     }
 }
